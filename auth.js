20c86240dfa89c2fbdece72c9c2586682cf6accf
Lim <sergeeffdanya@gmail.com>
2026-02-07 22:18:05 +0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -79,4 +79,3 @@ export function requireAuth({ next = "index.html", loginPage = "login.html" } = 
 
 // Удобный доступ как раньше (через window.__auth)
 window.__auth = { sha256Hex, login, logout, getSession, setSession, clearSession, requireAuth };
-window.__firebaseConfig = firebaseConfig;
