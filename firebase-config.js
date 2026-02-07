15d93fe170c716eec3ba8ef5c4fb4a20410c2f5a
Lim <sergeeffdanya@gmail.com>
2026-02-07 22:11:52 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// firebase-config.js
+// Конфиг Firebase оставлен только для Firestore (без Firebase Authentication).
+
+export const firebaseConfig = {
+  "apiKey": "AIzaSyCtxLMUG883UGtubHvH6V_418I8ga1eIpU",
+  "authDomain": "pipiska-d2fe6.firebaseapp.com",
+  "projectId": "pipiska-d2fe6",
+  "storageBucket": "pipiska-d2fe6.firebasestorage.app",
+  "messagingSenderId": "414167313955",
+  "appId": "1:414167313955:web:f0107175e3c942fbcb8b96",
+  "measurementId": "G-NSCEB7SW34"
+};
+
+window.__firebaseConfig = firebaseConfig;
