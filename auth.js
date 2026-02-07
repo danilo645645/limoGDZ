6a34b88b0b35767019a07a2a36b510c9532d7a3d
Lim <sergeeffdanya@gmail.com>
2026-02-07 19:29:49 +0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -3,11 +3,15 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebas
 import { getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
 
 // ВСТАВЬ СЮДА СВОЙ firebaseConfig ИЗ FIREBASE CONSOLE
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "…",
-  authDomain: "…",
-  projectId: "…",
-  appId: "…",
+  apiKey: "AIzaSyCtxLMUG883UGtubHvH6V_418I8ga1eIpU",
+  authDomain: "pipiska-d2fe6.firebaseapp.com",
+  projectId: "pipiska-d2fe6",
+  storageBucket: "pipiska-d2fe6.firebasestorage.app",
+  messagingSenderId: "414167313955",
+  appId: "1:414167313955:web:f0107175e3c942fbcb8b96",
+  measurementId: "G-NSCEB7SW34"
 };
 
 const app = initializeApp(firebaseConfig);
